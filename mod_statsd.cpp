0cda7daa5bd0215e06600f3ff8e0b99bedfbf22c
Kinshuk Bairagi <me@kinshuk.in>
2018-08-04 15:00:31 +0530

    basic impl statsd_cdr_reporting

--- mod_statsd.cpp
+++ mod_statsd.cpp
@@ -98,134 +98,46 @@ static int sql_count_callback(void *pArg, int argc, char **argv, char **columnNa
 
 static switch_status_t statsd_cdr_reporting(switch_core_session_t *session)
 {
-	// switch_channel_t *channel = switch_core_session_get_channel(session);
-	// switch_memory_pool_t *pool = switch_core_session_get_pool(session);
-	// switch_hash_index_t *hi;
-	// const void *var;
-	// void *val;
-	// switch_console_callback_match_t *matches = NULL;
-	// switch_console_callback_match_node_t *m;
-	// const char *uuid = NULL;
-	// int is_b;
+	try {
+		
+		switch_channel_t *channel = switch_core_session_get_channel(session);
+		switch_memory_pool_t *pool = switch_core_session_get_pool(session);
+		const char *uuid = NULL;
+		std::string event_name;
+		const char *tmp;
+		// int is_b;
+		// is_b = channel && switch_channel_get_originator_caller_profile(channel);
 
-	// is_b = channel && switch_channel_get_originator_caller_profile(channel);
+		if (!(uuid = switch_channel_get_variable(channel, "uuid"))) {
+			uuid = switch_core_strdup(pool, switch_core_session_get_uuid(session));
+		}
 
-	// if (globals.log_leg == ODBC_CDR_LOG_A && is_b) {
-	// 	switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_NOTICE, "Only logging A-Leg, ignoring B-leg\n");
-	// 	return SWITCH_STATUS_SUCCESS;
-	// } else if (globals.log_leg == ODBC_CDR_LOG_B && !is_b) {
-	// 	switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_NOTICE, "Only logging B-Leg, ignoring A-leg\n");
-	// 	return SWITCH_STATUS_SUCCESS;
-	// }  
+		// //hangup_cause
+		// //hold_accum_seconds
+		// //duration
+		// //answersec
+		// //sip_hangup_disposition
+		// //quality_percentage
 
-	// if (!(uuid = switch_channel_get_variable(channel, "uuid"))) {
-	// 	uuid = switch_core_strdup(pool, switch_core_session_get_uuid(session));
-	// }
+		tmp = switch_channel_get_variable(channel, "hangup_cause");
+		event_name = "hangup_cause."  + (std::string) tmp;
+		statsd_count(globals.link, event_name , 1, 1.0);
 
-	// // copy all table names from global hash
-	// switch_mutex_lock(globals.mutex);
-	// for (hi = switch_core_hash_first(globals.table_hash); hi; hi = switch_core_hash_next(&hi)) {
-	// 	switch_core_hash_this(hi, &var, NULL, &val);
-	// 	switch_console_push_match(&matches, (const char *) var);
-	// }
-	// switch_mutex_unlock(globals.mutex);
+		tmp = switch_channel_get_variable(channel, "duration");
+		statsd_gauge(globals.link, "call_duration" , atoi(tmp));
 
-	// //hangup_cause
-	// //hold_accum_seconds
-	// //duration
-	// //answersec
-	// //sip_hangup_disposition
-	// //quality_percentage
+		tmp = switch_channel_get_variable(channel, "answersec");
+		statsd_gauge(globals.link, "call_answer_seconds" , atoi(tmp));
 
-	// const char *tmp = switch_channel_get_variable(channel, field_hash_val->chan_var_name);
+		tmp = switch_channel_get_variable(channel, "hold_accum_seconds");
+		statsd_gauge(globals.link, "call_hold_seconds" , atoi(tmp));
 
+		return SWITCH_STATUS_SUCCESS;
 
-	// if (matches) {
-	// 	table_profile_t *table = NULL;
-
-	// 	// loop through table names
-	// 	for (m = matches->head; m; m = m->next) {
-	// 		char *table_name = m->val;
-	// 		switch_bool_t started = SWITCH_FALSE;
-	// 		switch_bool_t skip_leg = SWITCH_FALSE;
-
-	// 		switch_mutex_lock(globals.mutex);
-	// 		table = switch_core_hash_find(globals.table_hash, table_name);
-	// 		switch_mutex_unlock(globals.mutex);
-
-			 
-
-	// 		if (skip_leg == SWITCH_FALSE) {
-	// 			switch_hash_index_t *i_hi = NULL;
-	// 			const void *i_var;
-	// 			void *i_val;
-	// 			char *field_hash_key;
-	// 			cdr_field_t *field_hash_val;
-	// 			char *sql = NULL;
-	// 			char *full_path = NULL;
-	// 			switch_stream_handle_t stream_field = { 0 };
-	// 			switch_stream_handle_t stream_value = { 0 };
-	// 			switch_bool_t insert_fail = SWITCH_FALSE;
-
-	// 			SWITCH_STANDARD_STREAM(stream_field);
-	// 			SWITCH_STANDARD_STREAM(stream_value);
-
-	// 			for (i_hi = switch_core_hash_first_iter( table->field_hash, i_hi); i_hi; i_hi = switch_core_hash_next(&i_hi)) {
-	// 				const char *tmp;
-	// 				switch_core_hash_this(i_hi, &i_var, NULL, &i_val);
-	// 				field_hash_key = (char *) i_var;
-	// 				field_hash_val = (cdr_field_t *) i_val;
-	// 				tmp = switch_channel_get_variable(channel, field_hash_val->chan_var_name);
-
-	// 				if (!tmp && field_hash_val->default_value) {
-	// 					tmp = field_hash_val->default_value;
-	// 				}
-
-	// 				if (tmp) {
-	// 					if (started == SWITCH_TRUE) {
-	// 						stream_field.write_function(&stream_field, ", ");
-	// 						stream_value.write_function(&stream_value, ", ");
-	// 					}
-
-	// 					stream_field.write_function(&stream_field, "%q", field_hash_key);
-
-	// 					if (field_hash_val->quote) {
-	// 						stream_value.write_function(&stream_value, "'%q'", tmp);
-	// 					} else {
-	// 						stream_value.write_function(&stream_value, "%q", tmp);
-	// 					}
-
-	// 					started = SWITCH_TRUE;
-	// 				}
-
-	// 			}
-	// 			switch_safe_free(i_hi);
-
-	// 			sql = switch_mprintf("INSERT INTO %q (%s) VALUES (%s)", table_name, stream_field.data, stream_value.data);
-	// 			if (globals.debug_sql == SWITCH_TRUE) {
-	// 				switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_DEBUG, "sql %s\n", sql);
-	// 			}
-	// 			if (odbc_cdr_execute_sql_no_callback(sql) != SWITCH_STATUS_SUCCESS) {
-	// 				insert_fail = SWITCH_TRUE;
-	// 				switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_ERROR, "Error executing query %s\n", sql);
-	// 			}
- 
-
-	// 			switch_safe_free(sql);
-
-	// 			switch_safe_free(stream_field.data);
-	// 			switch_safe_free(stream_value.data);
-
-	// 		}
-
-	// 	}
-
-	// 	switch_console_free_matches(&matches);
-	// }
-
-	// switch_safe_free(hi);
-
-	return SWITCH_STATUS_SUCCESS;
+	} catch(...) { 
+        switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Error running statsd_cdr_reporting\n");
+        return SWITCH_STATUS_SUCCESS;
+    }
 }
 
 switch_state_handler_table_t statsd_state_handlers = {
